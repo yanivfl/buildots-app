82a60e8586b8b9be24e545615f7ffa84c1c2da06
Yaniv Fleischer <58190248+yanivfl@users.noreply.github.com>
2022-10-05 17:42:33 +0300

    my first commit!

--- src/components/Pages/ExplorerPage/ExplorerPage.js
+++ src/components/Pages/ExplorerPage/ExplorerPage.js
@@ -6,23 +6,24 @@ import { Panorama } from './Panorama';
 
 const ExplorerPage = () => {
   const [form] = Form.useForm();
-  const { apartments, loading } = useExplorerStore();
+  const { apartments, loading, error } = useExplorerStore();
   const date = form.getFieldValue('date');
   const apartmentName = form.getFieldValue('apartmentName');
   const image = getImage({ date, apartmentName, apartments });
   const emptyImageMessage = loading ? 'Loading...' : 'Please select a date';
 
-  return apartments && (
-    <FullSizeBox flexDirection="column">
-      <FullSizeBox height={35}>
-        <ExplorerTopBar form={form} apartments={apartments} />
+  return error
+    ? 'error' : (
+      <FullSizeBox flexDirection="column">
+        <FullSizeBox height={35}>
+          <ExplorerTopBar form={form} apartments={apartments} />
+        </FullSizeBox>
+        <FullSizeBox flex={1} padding={20} justifyContent="center" alignItems="center">
+          {image ? <Panorama url={image.url} id={image.id} />
+            : emptyImageMessage}
+        </FullSizeBox>
       </FullSizeBox>
-      <FullSizeBox flex={1} padding={20} justifyContent="center" alignItems="center">
-        {image ? <Panorama url={image.url} id={image.id} />
-          : emptyImageMessage}
-      </FullSizeBox>
-    </FullSizeBox>
-  );
+    );
 };
 
 export { ExplorerPage };
